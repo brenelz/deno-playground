8a915bc164dd5690286e59b4f2eac65d1735bf25
Brenelz <brenleydueck@gmail.com>
2022-05-31 20:19:17 -0500

    h = createElement

--- server.tsx
+++ server.tsx
@@ -1,4 +1,4 @@
-// @jsx createElement
+/** @jsx createElement */
 import { serve } from "https://deno.land/std@0.119.0/http/server.ts";
 import { bundle } from "https://deno.land/x/emit/mod.ts";
 import { ReactDomServer, React, StaticRouter, createElement } from "./dep.ts";
